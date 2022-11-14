aac79f694cd71e748ea192eb5fdc21480e172af7
louis nicoletti <louis.nicoletti@gmail.com>
2022-11-14 14:07:11 +0100

    stupid coach is back

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  Rails.application.routes.draw do
+    root to: "pages#home"
+    get "question", to: "questions#question"
+    get "ask", to: "questions#ask"
+    get "answer", to: "questions#answer"
+  end
   # root "articles#index"
-end
